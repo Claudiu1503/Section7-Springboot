3d92648f2657b0041bfc1b716056249535d850a3
Claudiu_1503 <adichis1503@yahoo.com>
2024-09-04 23:11:15 +0300

    Full section 6

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6restmvc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerTest {
+
+    @Autowired
+    BeerController beerController;
+
+
+
+
+    @Test
+    void geetBeerById() {
+
+        System.out.println(beerController.geetBeerById(UUID.randomUUID()));
+
+    }
+}
