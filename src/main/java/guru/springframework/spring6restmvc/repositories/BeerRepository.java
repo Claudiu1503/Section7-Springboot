df06e6f1f66b1db0146ad61c89a58c357a2df807
Claudiu_1503 <adichis1503@yahoo.com>
2024-09-18 17:15:02 +0300

    151 - Find by name + test Implemenntation

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -4,7 +4,10 @@ import guru.springframework.spring6restmvc.entities.Beer;
 import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
